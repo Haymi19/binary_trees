f278b95dfa0af5c6090f1b3537e0319f948cc4d0
Haimanot Eshete <haimanoteshete685@gmail.com>
2023-03-30 10:56:47 -0700

    tree depth

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,5 +13,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	else
-		return (binary_tree_height(tree->left), binary_tree_height(tree->right) + 1);
+	{
+		size_t l = 0, r = 0;
+
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
+	}
 }
